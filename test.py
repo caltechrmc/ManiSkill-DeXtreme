7419592f57e553fbfecffd90d29b638ccfc22e95
Ryan Rudes <ryanrudes@gmail.com>
2025-03-29 00:38:26 -0400

    Set reward mode to dense normalized

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from tasks.reorient_cube import *
 import gymnasium as gym
 import numpy as np
 
-env = gym.make("ReorientCube-v0", render_mode="human", reward_mode="sparse")
+env = gym.make("ReorientCube-v0", render_mode="human")
 env.reset()
 
 while True:
